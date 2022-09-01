f08f482899c577981adb949dcf532baaf694701d
Pedro Soares <62397830+pedroslvieira@users.noreply.github.com>
2022-09-01 10:38:35 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,11 @@ Notification.destroy_all
 User.destroy_all
 
 # Creating Admins
-inyova_emails = ["sergi@inyova.com", "lollija@inyova.com", "rafael@inyova.com", "simon@inyova.com"]
-inyova_emails.each { |email| User.create!(email: email, password: "password", admin: true) }
+dev_emails = ["john@example.com", "paul@example.com"]
+dev_emails.each { |email| User.create!(email: email, password: "password", admin: true) }
 
 # Creating Clients
-john = User.create!(email: "john@gmail.com", password: "password")
+carl = User.create!(email: "carl@gmail.com", password: "password")
 maria = User.create!(email: "maria@gmail.com", password: "password")
 marcelo = User.create!(email: "marcelo@gmail.com", password: "password")
 luiza = User.create!(email: "luiza@gmail.com", password: "password")
@@ -17,21 +17,21 @@ Notification.create!(
   title: "Welcome",
   description: "You just became a user in Notification Center API"
 )
-Notification.last.users = [john, maria, marcelo, luiza]
+Notification.last.users = [carl, maria, marcelo, luiza]
 
 Notification.create!(
   date: DateTime.now,
   title: "New message",
   description: "You just received a new message from Maria"
 )
-Notification.last.users = [john]
+Notification.last.users = [carl]
 
 Notification.create!(
   date: DateTime.now,
   title: "New post from Nasa",
   description: "Nasa has a new post that is trending"
 )
-Notification.last.users = [john, maria, luiza]
+Notification.last.users = [carl, maria, luiza]
 
 Notification.create!(
   date: DateTime.now,
